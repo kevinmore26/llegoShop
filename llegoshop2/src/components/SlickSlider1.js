6ce7768d506ef4b1ec78f4cff9090a5f9bac43a9
Kevin More <74431425+kevinmore26@users.noreply.github.com>
2022-05-30 21:22:05 -0500

    commit

--- llegoshop2/src/components/SlickSlider1.js
+++ llegoshop2/src/components/SlickSlider1.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Card from "./Card"
+import bebidas from '../assets/bebidas.png'
 import Slider from "react-slick";
-import tarjeta1 from '../assets/1.jpeg'
-import tarjeta2 from '../assets/1.jpeg'
-import tarjeta3 from '../assets/1.jpeg'
-import tarjeta4 from '../assets/1.jpeg'
+import electro from '../assets/electro.png'
+import envios from '../assets/envios.png'
+import farmacia from '../assets/farmacia.png'
+import canasta from '../assets/canasta.png'
+import mascotas from '../assets/mascotas.png'
 import {Link} from "react-router-dom"
+import './slickSlider.css'
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 export default function SimpleSlider() {
@@ -13,39 +16,57 @@ export default function SimpleSlider() {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToShow: 6,
+        slidesToScroll: 6
       };
     return (
-        <div>
-        <h2> Multiple items </h2>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
+        <div style={{marginTop:'60px',with:'98%'}}>
+        
+        <Slider {...settings} style={{width:'98%'}}>
+          <div className="bordeCarrousel">
+            <div>
+            <h4 style={{textAlign:'center'}}>Bebidas </h4>
+          <img src={bebidas} style={{height:'120px',margin:'0 auto' }} alt="perrito"></img></div>
           </div>
-          <div>
-            <h3>2</h3>
+          <div className="bordeCarrousel">
+          <h4 style={{textAlign:'center'}}>Electro </h4>
+          <img src={electro} style={{height:'120px',margin:'0 auto' }} alt="perrito"></img>
           </div>
-          <div>
-            <h3>3</h3>
+          <div className="bordeCarrousel">
+          <h4 style={{textAlign:'center'}}>Farmacia </h4>
+          <img src={farmacia} style={{height:'120px',margin:'0 auto' }} alt="perrito"></img>
           </div>
-          <div>
-            <h3>4</h3>
+          <div className="bordeCarrousel">
+          <h4 style={{textAlign:'center'}}>Envíos </h4>
+          <img src={envios} style={{height:'120px',margin:'0 auto' }} alt="perrito"></img>
           </div>
-          <div>
-            <h3>5</h3>
+          <div className="bordeCarrousel">
+          <h4 style={{textAlign:'center'}}>Mascotas </h4>
+          <img src={mascotas} style={{height:'120px',margin:'0 auto' }} alt="perrito"></img>
           </div>
-          <div>
-            <h3>6</h3>
+          <div className="bordeCarrousel">
+          <h4 style={{textAlign:'center'}}>Canasta </h4>
+          <img src={canasta} style={{height:'120px',margin:'0 auto' }} alt="perrito"></img>
           </div>
-          <div>
-            <h3>7</h3>
+          <div className="bordeCarrousel">
+            <h4 style={{textAlign:'center'}}>Bebidas </h4>
+          <img src={bebidas} style={{height:'120px',margin:'0 auto' }} alt="perrito"></img>
           </div>
-          <div>
-            <h3>8</h3>
+          <div className="bordeCarrousel">
+          <h4 style={{textAlign:'center'}}>Electro </h4>
+          <img src={electro} style={{height:'120px',margin:'0 auto' }} alt="perrito"></img>
           </div>
-          <div>
-            <h3>9</h3>
+          <div className="bordeCarrousel">
+          <h4 style={{textAlign:'center'}}>Farmacia </h4>
+          <img src={farmacia} style={{height:'120px',margin:'0 auto' }} alt="perrito"></img>
+          </div>
+          <div className="bordeCarrousel">
+          <h4 style={{textAlign:'center'}}>Envíos </h4>
+          <img src={envios} style={{height:'120px',margin:'0 auto' }} alt="perrito"></img>
+          </div>
+          <div className="bordeCarrousel">
+          <h4 style={{textAlign:'center'}}>Mascotas </h4>
+          <img src={mascotas} style={{height:'120px',margin:'0 auto' }} alt="perrito"></img>
           </div>
         </Slider>
       </div>
