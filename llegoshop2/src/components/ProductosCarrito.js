8c8f5ae30bf622ff4c61fed9913a674a5170b2bd
Kevin More <74431425+kevinmore26@users.noreply.github.com>
2022-07-11 22:29:25 -0500

    commit

---
+++ llegoshop2/src/components/ProductosCarrito.js
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default function GroupProductsCarrito({ carrito }) {
+  let total = 0;
+  total = carrito.reduce((acum, item) => {
+    return acum + item.cantidad * item.precio;
+  }, 0);
+  let totalComprado = 0;
+  totalComprado = carrito.reduce((acum, item) => {
+    return acum + item.cantidad;
+  }, 0);
+
+  return (
+    <Container style={{ position: "relative", top: "100px" }}>
+      <div style={{ display: "flex" }}>
+        <div style={{ width: "80%", padding: "30px" }}>
+          <h4 style={{ fontWeight: "bold" }}>CARRITO DE COMPRAS</h4>
+          <hr></hr>
+          {carrito.map((prod, i) => (
+            <div
+              style={{ display: "flex", padding: "10px" }}
+              key={i}
+              className="row"
+            >
+              <div className="col-3">
+                <img
+                  src={prod.productoImagen}
+                  alt={prod.productoImagen}
+                  className="card-img-top"
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    margin: "0 auto",
+                  }}
+                />
+              </div>
+
+              <div className="col-6">
+                <h5>{prod.nombre}</h5>
+                <p>{prod.descripcion}</p>
+                <h4 style={{ fontWeight: "bold", color: "#4F79C1" }}>
+                  S/{prod.precio}
+                </h4>
+              </div>
+              <div className="col-3">
+                <p>Cantidad</p>
+                <h6>{prod.cantidad}</h6>
+              </div>
+              <hr></hr>
+            </div>
+          ))}
+        </div>
+        <div
+          style={{
+            width: "20%",
+            border: "0.5px solid #00000026",
+            padding: "30px",
+          }}
+        >
+          <div style={{ textAlign: "center", fontWeight: "bold" }}>
+            RESUMEN DE LA ORDEN
+          </div>
+          <hr></hr>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <p>SubTotal({totalComprado})</p>
+            <p>S/{total}</p>
+          </div>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <h6 style={{ fontWeight: "bold" }}>TOTAL </h6>
+            <h6 style={{ fontWeight: "bold" }}>S/{total}</h6>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+}
--- llegoshop2/src/views/CarritoView.js
+++ llegoshop2/src/views/CarritoView.js
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import NavTop from "../components/NavTop";
+import GroupProductsCarrito from "../components/ProductosCarrito";
 import { CarritoContext } from "../context/carritoContext";
 
 export default function CarritoView() {
@@ -8,45 +9,11 @@ export default function CarritoView() {
   return (
     <div className="container">
       <NavTop />
-      <div style={{ position: "relative", top: "50px" }}>
-        <div className="my-4 text-center">Resumen de compra</div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>ImagenReferencial</th>
-              <th>Nombre</th>
-              <th>Cantidad</th>
-              <th>Precio c/u</th>
-              <th>Precio x producto</th>
-             
-            </tr>
-          </thead>
-          <tbody>
-            {carrito.map((prod, i) => ( 
-              <tr key={i}>
-                <td>
-                  <img
-                    src={
-                      prod.productoImagen !== ""
-                        ? prod.productoImagen
-                        : "https://upload.wikimedia.org/wikipedia/commons/0/0a/No-image-available.png"
-                    }
-            
-                    style={{ width: "100px" }}
-                    alt="error"
-                  />
-                </td>
-                <td>{prod.nombre}</td>
 
-                <td>{prod.cantidad}</td>
-                <td>{prod.precio}</td>
-                <td>{prod.cantidad * prod.precio}</td>
-           
-              </tr> 
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <GroupProductsCarrito
+        carrito={carrito}
+        style={{ position: "relative", top: "150px !important" }}
+      />
     </div>
   );
 }
